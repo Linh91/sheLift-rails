6e8b98c96c796044d5b1fa10f34ae529532e0776
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-07-23 20:39:13 +0100

    Test using assert_method for HTML tags

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,15 +4,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | sheLifts"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | sheLifts"
   end
 
   test "Should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | sheLifts"
   end
 end
